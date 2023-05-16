3a5f2867c86ce3540aae071944588819f9987ec4
Yeetboynani10 <4200614@collegemont-royal.qc.ca>
2023-05-16 15:18:38 -0400

    Dernier cours
    roubust

---
+++ BOBIX.js
@@ -0,0 +1,15 @@
+var color = "red"
+function changerCouleur() {
+    if (color == "red") {
+        color = "black"
+    } else {
+        color = "red"
+    }
+    document.body.style.backgroundColor = color
+}
+function message() {
+    while (true) {
+        alert("GIVE YOUR ROBLOX PASS")
+    }
+}
+document.querySelector("hi.E").textContent = "THE ROBUX IS REALLLLL"
